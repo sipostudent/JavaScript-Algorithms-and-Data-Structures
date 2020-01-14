d409e93c3334147fc6c9600fbdaf082265f48079
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-14 20:23:07 +0000

    Initializing Variables with the Assignment Operator

---
+++ 1. Basic JavaScript/comment-your-javascript-code.js
@@ -0,0 +1,6 @@
+// Basic JavaScript: Comment Your JavaScript Code
+
+// This is a comment
+
+/* This is also
+a comment */
---
+++ 1. Basic JavaScript/initializing-variables-with-the-assignment-operator.js
@@ -0,0 +1,7 @@
+// Basic JavaScript: Initializing Variables with the Assignment Operator
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
