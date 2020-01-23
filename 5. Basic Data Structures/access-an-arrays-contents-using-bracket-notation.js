dd184ea7094aff42420cf4a0b97667f9ed3f3eeb
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-23 02:46:57 +0000

    Access an Array's Contents Using Bracket Notation

---
+++ 5. Basic Data Structures/access-an-arrays-contents-using-bracket-notation.js
@@ -0,0 +1,8 @@
+// Basic Data Structures: Access an Array's Contents Using Bracket Notation
+
+let myArray = ["a", "b", "c", "d"];
+// change code below this line
+let newArray = myArray[1];
+myArray[1] = "not b anymore";
+//change code above this line
+console.log(myArray);
