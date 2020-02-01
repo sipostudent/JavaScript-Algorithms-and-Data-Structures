30dba364cdce079f0b8fceb37c2e08c8155dfe19
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-01 05:38:09 +0000

    Reverse a String

--- 6. Basic Algorithm Scripting/reverse-a-string.js
+++ 6. Basic Algorithm Scripting/reverse-a-string.js
@@ -19,8 +19,27 @@
     ----------------------------------
 */
 
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// console.log(reverseString('hello'));
+// console.log(reverseString('Howdy'));
+// console.log(reverseString('Greetings from Earth'));
+
+/*
+    Challenge Solution (using Algorithm Scripting)
+    ------------------------------------------------
+*/
+
 function reverseString(str) {
-  return str.split('').reverse().join('');
+  let result = '';
+
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    result += str[i];
+  }
+
+  return result;
 }
 
 console.log(reverseString('hello'));
