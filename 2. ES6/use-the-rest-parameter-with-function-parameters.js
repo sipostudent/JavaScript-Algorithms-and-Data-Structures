2a025d4ff2356fb68f6a28c2d9a0ca62685caef8
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-09 06:01:00 +0000

    Use the Rest Parameter with Function Parameters

---
+++ 2. ES6/use-the-rest-parameter-with-function-parameters.js
@@ -0,0 +1,58 @@
+/*
+    ES6
+    ===
+*/
+
+/*
+    Use the Rest Parameter with Function Parameters
+    -----------------------------------------------
+
+    In order to help us create more flexible functions, ES6 introduces 
+    the rest parameter for function parameters. With the rest parameter, 
+    you can create functions that take a variable number of arguments. 
+    These arguments are stored in an array that can be accessed later 
+    from inside the function.
+
+    Check out this code:
+
+        function howMany(...args) {
+        return "You have passed " + args.length + " arguments.";
+        }
+        console.log(howMany(0, 1, 2)); // You have passed 3 arguments.
+        console.log(howMany("string", null, [1, 2, 3], { })); // You 
+        have passed 4 arguments.
+    
+    The rest parameter eliminates the need to check the args array and 
+    allows us to apply map(), filter() and reduce() on the parameters array.
+
+    
+    Challenge
+    ---------
+
+    Modify the function sum using the rest parameter in such a way that 
+    the function sum is able to take any number of arguments and return their sum.
+
+
+    Expected Results
+    ----------------
+
+    The result of sum(0,1,2) should be 3
+
+    The result of sum(1,2,3,4) should be 10
+
+    The result of sum(5) should be 5
+
+    The result of sum() should be 0
+
+    The sum function should use the ... rest parameter on the args parameter.
+*/
+
+/*
+    Challenge Solution
+    ------------------
+*/
+
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+}
+console.log(sum(1, 2, 3)); // 6
