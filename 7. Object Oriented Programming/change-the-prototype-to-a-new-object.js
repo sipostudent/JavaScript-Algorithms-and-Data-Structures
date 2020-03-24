dc0106b9ee4192dbbbfdbe376478f957bbddaa25
Sipo Charles <sipocharles@yahoo.co.uk>
2020-03-24 23:58:32 +0000

    Change the Prototype to a New Object

--- 7. Object Oriented Programming/change-the-prototype-to-a-new-object.js
+++ 7. Object Oriented Programming/change-the-prototype-to-a-new-object.js
@@ -49,14 +49,14 @@
 
 function Dog(name) {
     this.name = name;
-  }
-  Dog.prototype = {
+}
+Dog.prototype = {
     // Add your code below this line
     numLegs: 2,
-    eat: function() {
-      console.log("nom nom nom");
+    eat: function () {
+        console.log("nom nom nom");
     },
-    describe: function() {
-      console.log("My name is " + this.name);
+    describe: function () {
+        console.log("My name is " + this.name);
     }
-  };
+};
